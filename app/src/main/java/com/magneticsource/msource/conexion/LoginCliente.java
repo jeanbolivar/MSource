fdf16ed6e2da7cf745edb572308de9e5e5bfb236
Emanuel <riveraemanuel2014@gmail.com>
2015-11-28 17:36:34 -0500

    LoginCliente

--- app/src/main/java/com/magneticsource/msource/conexion/LoginCliente.java
+++ app/src/main/java/com/magneticsource/msource/conexion/LoginCliente.java
@@ -1,5 +1,19 @@
 package com.magneticsource.msource.conexion;
 
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.magneticsource.msource.R;
+import com.magneticsource.msource.control.Alumno;
+import com.magneticsource.msource.control.Datos;
+import com.magneticsource.msource.control.Persona;
+import com.magneticsource.msource.control.Profesor;
+import com.magneticsource.msource.ui.AlumnoActivity;
+import com.magneticsource.msource.ui.ProfesorActivity;
+
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.ksoap2.serialization.SoapObject;
 
 /**
@@ -16,7 +30,20 @@ public class LoginCliente extends ServicioWebCliente {
 
 		request.addProperty(crearPropiedad("dni", dni, String.class));
 		request.addProperty(crearPropiedad("clave", clave, String.class));
+		String s = getString(Metodo, request, URL);
 
-		return getBoolean(Metodo, request, URL);
+		JSONParser parser = new JSONParser();
+		Object obj = null;
+        Boolean b = null;
+		try {
+			obj = parser.parse(s);
+			JSONArray array = (JSONArray)obj;
+            b = (Boolean)array.get(0);
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return b;
 	}
 }
